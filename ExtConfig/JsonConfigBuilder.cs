21fa509d0ffa7b338f33560bb26aee2062531e59
shumutun <>
2023-02-26 12:28:01 +0100

    Version 3.0.0

--- ExtConfig/JsonConfigBuilder.cs
+++ ExtConfig/JsonConfigBuilder.cs
@@ -1,15 +1,17 @@
-﻿using ExtConfig.VariablesSources;
+﻿using System;
+using ExtConfig.VariablesSources;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
 
 namespace ExtConfig
 {
     public static class JsonConfigBuilder
     {
         private const string _include = "_include";
-        private const string _variables_source = "_variables_source";
+        private const string _variablesSource = "_variables_source";
         private static readonly Regex _envVariableRegex = new Regex(@"\$\{([-_,A-Z,0-9]+)\}");
 
         private static readonly JsonMergeSettings _jsonMergeSettings = new JsonMergeSettings
@@ -18,64 +20,98 @@ namespace ExtConfig
             MergeArrayHandling = MergeArrayHandling.Union
         };
 
-        public static T Build<T>(string filePattern)
+        public static T? Build<T>(string filePattern)
             where T : class
         {
             var filePath = Directory.GetFiles(Directory.GetCurrentDirectory(), filePattern).SingleOrDefault();
+            if (filePath == null)
+                throw new ArgumentException("No files found matching the file pattern");
             var obj = JObject.Parse(File.ReadAllText(filePath));
-            return Build<T>(obj, VariablesSource.GetConfigVariables(obj.Value<string>(_variables_source)));
+            return Build<T>(obj, VariablesSource.GetConfigVariables(obj.Value<string>(_variablesSource)));
         }
 
-        public static T Build<T>(JObject obj, IConfigVariables enviromentVariables)
+        public static T? Build<T>(IConfigurationSection config, IConfigVariables environmentVariables)
+            where T : class
+        {
+            var obj = Serialize(config);
+            return Build<T>(obj, environmentVariables);
+        }
+
+        private static JObject Serialize(IConfiguration config)
+        {
+            var obj = new JObject();
+            foreach (var child in config.GetChildren())
+                obj.Add(child.Key, Serialize(child));
+            if (obj.HasValues || config is not IConfigurationSection section) 
+                return obj;
+            if (section.Value == null)
+                throw new ArgumentException("The config is empty");
+            return JObject.Parse(section.Value);
+        }
+
+        public static T? Build<T>(JObject obj, IConfigVariables environmentVariables)
             where T : class
         {
             IncludeSubConfigs(obj);
-            Transform(obj, enviromentVariables);
+            Transform(obj, environmentVariables);
             return obj.ToObject<T>();
         }
 
         private static void IncludeSubConfigs(JObject obj)
         {
-            if (obj.TryGetValue(_include, out JToken include))
-            {
-                var filepath = include.Value<string>();
-                var includeObj = JObject.Parse(File.ReadAllText(filepath));
-                IncludeSubConfigs(includeObj);
-                obj.Merge(includeObj, _jsonMergeSettings);
-            }
+            if (!obj.TryGetValue(_include, out JToken? include)) 
+                return;
+            var filepath = include.Value<string>();
+            if (filepath == null)
+                throw new ArgumentNullException(_include);
+            var includeObj = JObject.Parse(File.ReadAllText(filepath));
+            IncludeSubConfigs(includeObj);
+            obj.Merge(includeObj, _jsonMergeSettings);
         }
 
-        private static void Transform(JObject obj, IConfigVariables enviromentVariables)
+        private static void Transform(JObject obj, IConfigVariables environmentVariables)
         {
             foreach (var item in obj)
-                if (item.Value?.Type == JTokenType.Object)
-                    Transform((JObject)item.Value, enviromentVariables);
-                else if (item.Value?.Type == JTokenType.Array)
-                    foreach (var arrayItem in item.Value)
-                        if (arrayItem is JObject)
-                            Transform((JObject)arrayItem, enviromentVariables);
-                        else
-                            EnvVariableSubstitution(arrayItem, enviromentVariables);
-                else
-                    EnvVariableSubstitution(item.Value, enviromentVariables);
+                switch (item.Value?.Type)
+                {
+                    case JTokenType.Object:
+                    {
+                        Transform((JObject)item.Value, environmentVariables);
+                        break;
+                    }
+                    case JTokenType.Array:
+                    {
+                        foreach (var arrayItem in item.Value)
+                            if (arrayItem is JObject jObject)
+                                Transform(jObject, environmentVariables);
+                            else
+                                EnvVariableSubstitution(arrayItem, environmentVariables);
+                        break;
+                    }
+                    default:
+                    {
+                        EnvVariableSubstitution(item.Value, environmentVariables);
+                        break;
+                    }
+                }
         }
 
-        private static void EnvVariableSubstitution(JToken item, IConfigVariables enviromentVariables)
+        private static void EnvVariableSubstitution(JToken? item, IConfigVariables environmentVariables)
         {
             if (item?.Type != JTokenType.String)
                 return;
-            var val = item.Value<string>();
-            if (_envVariableRegex.IsMatch(val))
-            {
-                foreach (Match match in _envVariableRegex.Matches(val))
-                    if (match != null)
-                    {
-                        var envName = match.Groups[0].Value;
-                        var envValue = enviromentVariables.GetEnviromentVariable(match.Groups[1].Value);
-                        val = val.Replace(envName, envValue);
-                    }
-                item.Replace(JToken.FromObject(val));
-            }
+            var value = item.Value<string>()!;
+            if (!_envVariableRegex.IsMatch(value))
+                return;
+            foreach (Match match in _envVariableRegex.Matches(value))
+                if (match != null)
+                {
+                    var envName = match.Groups[0].Value;
+                    var envValue = environmentVariables.GetEnviromentVariable(match.Groups[1].Value);
+                    value = value.Replace(envName, envValue);
+                }
+
+            item.Replace(JToken.FromObject(value));
         }
     }
 }
--- ExtConfig/VariablesSources/VariablesSource.cs
+++ ExtConfig/VariablesSources/VariablesSource.cs
@@ -1,24 +1,16 @@
-﻿using ExtConfig.VariablesSources.AsureKeyVault;
-using ExtConfig.VariablesSources.EnviromentVariable;
+﻿using ExtConfig.VariablesSources.EnviromentVariable;
 using System;
 
 namespace ExtConfig.VariablesSources
 {
     public static class VariablesSource
     {
-        private const string _enviromentvariables = "enviromentvariables";
-        private const string _asurekeyvault = "asurekeyvault:";
-
-
+        private const string _environmentVariables = "enviromentvariables";
+        
         public static IConfigVariables GetConfigVariables(string variablesSource)
         {
-            if (variablesSource == null || variablesSource.ToLower() == _enviromentvariables)
+            if (variablesSource == null || variablesSource.ToLower() == _environmentVariables)
                 return new EnvVariables();
-
-            if (variablesSource.ToLower().StartsWith(_asurekeyvault))
-                if (Uri.TryCreate(variablesSource.Substring(_asurekeyvault.Length), UriKind.Absolute, out Uri akvUri))
-                    return new AsureKeyVaultVariables(akvUri);
-
             throw new NotImplementedException();
         }
     }
